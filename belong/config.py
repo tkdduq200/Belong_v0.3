cca1b0142830f3a42a53e42145e693f15685d6bf
freenu0618 <freenu0618@naver.com>
2025-11-19 16:29:58 +0900

    oracle경로 재 지정

--- belong/config.py
+++ belong/config.py
@@ -4,7 +4,8 @@ import os
 
 BASE_DIR = os.path.dirname(__file__)
 
-SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'belong.db')}"
+SQLALCHEMY_DATABASE_URI = "oracle+cx_oracle://scott:tiger@localhost:1521/xe"
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SECRET_KEY = "dev-secret-key"
